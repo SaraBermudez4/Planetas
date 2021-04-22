b3e406d1611079e35b644fadd89a6fe04c330a9c
Sara Bermudez <bermudezalvarezsaraestefania@gmail.com>
2021-04-22 16:00:21 -0500

    Mejora en las funciones utilizando funciones de arreglos

--- main.js
+++ main.js
@@ -1,14 +1,11 @@
-var btn_ver = document.getElementById("btn_ver").addEventListener("click", traerdatos);
-
+//var btn_ver = document.getElementById("btn_ver").addEventListener("click", traerdatos);
 var btn_menor = document.getElementById("btn_menor").addEventListener("click", datos_menor);
-
 var btn_mayor = document.getElementById("btn_mayor").addEventListener("click", datos_mayor);
-
 var btn_33 = document.getElementById("btn_33").addEventListener("click", datos_33);
-
 var btn_nombre = document.getElementById("btn_nombre").addEventListener("click", buscar_nombre);
-
 var btn_orden_nombre = document.getElementById("btn_orden_nombre").addEventListener("click", datos_nombre);
+crearJson()
+traerdatos()
 
 function crearJson() {
     const planetas = new XMLHttpRequest();
@@ -33,7 +30,6 @@ function verDatos(datos) {
     `
     }
 }
-
 
 function traerdatos() {
 
@@ -81,7 +77,6 @@ function datos_menor() {
             const datos = JSON.parse(this.responseText);
             const planetaMenor = datos;
             planetaMenor.sort((a, b) => a.distanciaSol - b.distanciaSol)
-
             verDatos(planetaMenor);
         }
     }
@@ -93,158 +88,32 @@ function datos_33() {
     planetas.onreadystatechange = function () {
         if (this.readyState == 4 && this.status == 200) {
             const datos = JSON.parse(this.responseText);
-            let res = document.getElementById("res");
-            res.innerHTML = '';
-            for (let item of datos) {
-                const distancia = item.distanciaTierra;
-                for (i = 0; i < distancia.length; i++) {
-                    if (distancia[i] == "9") {
-                        if (distancia[i + 1] == "1") {
-                            res.innerHTML += `
-                            <tr>
-                                <td>${item.nombre}</td>
-                                <td><img src=${item.imagen}></td>
-                                <td>${item.distanciaSol}</td>
-                                <td>${item.distanciaTierra}</td>
-                                <td>${item.superficie}</td>
-                                <td>${item.masa}</td>
-                            </tr>
-                        `
-                        }
-                    }
-                }
-            }
-
+            var distancia1 = datos.filter(({ distanciaTierra }) => distanciaTierra.includes(91))
+            verDatos(distancia1)
         }
     }
 }
 
 function buscar_nombre() {
 
-    var nombre = document.getElementById("txt_name").value;
-    nombre = nombre.toLocaleLowerCase();
+    var txt_name = document.getElementById("txt_name").value;
+    txt_name = txt_name.toLocaleLowerCase();
     planetas = crearJson();
     planetas.onreadystatechange = function () {
         if (this.readyState == 4 && this.status == 200) {
             const datos = JSON.parse(this.responseText);
-            let res = document.getElementById("res");
+            var name = datos.find(({ nombre }) => nombre.toLowerCase() == txt_name)
             res.innerHTML = '';
-            for (let item of datos) {
-                if (item.nombre === nombre) {
-                    res.innerHTML += `
-                        <tr>
-                            <td>${item.nombre}</td>
-                            <td><img src=${item.imagen}></td>
-                            <td>${item.distanciaSol}</td>
-                            <td>${item.distanciaTierra}</td>
-                            <td>${item.superficie}</td>
-                            <td>${item.masa}</td>
-                        </tr>
-                    `
-                }
-            }
+            res.innerHTML += `
+            <tr>
+            <td>${name.nombre}</td>
+            <td><img src=${name.imagen}></td>
+            <td>${name.distanciaSol}</td>
+            <td>${name.distanciaTierra}</td>
+            <td>${name.superficie}</td>
+            <td>${name.masa}</td>
+        </tr>
+        `
         }
     }
 }
-
-/*
-for (i = 1; i < datos.length; i++) {
-    for (j = 0; j < (datos.length - i); j++) {
-        if (datos.distanciaSol[j] < datos.distanciaSol[j + 1]) {
-            aux = datos.distanciaSol[j];
-            datos.distanciaSol[j] = datos.distanciaSol[j + 1];
-            datos.distanciaSol[j + 1] = aux;
-
-            aux1 = nombre_planetas[j];
-            nombre_planetas[j] = nombre_planetas[j + 1];
-            nombre_planetas[j + 1] = aux1;
-
-            aux2 = tamaño_planetas[j];
-            tamaño_planetas[j] = tamaño_planetas[j + 1];
-            tamaño_planetas[j + 1] = aux2;
-
-            aux3 = distancia_tierra_planetas[j];
-            distancia_tierra_planetas[j] = distancia_tierra_planetas[j + 1];
-            distancia_tierra_planetas[j + 1] = aux3;
-        }
-    }
-}
-
-distancia_sol_planetas = new Array(13);
-tamaño_planetas = new Array(13);
-distancia_tierra_planetas = new Array(13);
-nombre_planetas = new Array(13);
-
-for (let i = 0; i < nombre_planetas.length; i++) {
-    nombre_planetas[i] = prompt('Ingrese nombre del planeta:', '');
-    tamaño_planetas[i] = prompt('Ingrese tamaño del planeta:', '');
-    distancia_sol_planetas[i] = prompt('¿Cual es la distancia que tiene del sol?:', '');
-    distancia_tierra_planetas[i] = prompt('¿Cual es la distancia que tiene de la tierra?:', '');
-}
-
-document.write('Mayor a menor ');
-
-for (i = 1; i < nombre_planetas.length; i++) {
-    for (j = 0; j < (nombre_planetas.length - i); j++) {
-        if (distancia_sol_planetas[j] < distancia_sol_planetas[j + 1]) {
-            aux = distancia_sol_planetas[j];
-            distancia_sol_planetas[j] = distancia_sol_planetas[j + 1];
-            distancia_sol_planetas[j + 1] = aux;
-
-            aux1 = nombre_planetas[j];
-            nombre_planetas[j] = nombre_planetas[j + 1];
-            nombre_planetas[j + 1] = aux1;
-
-            aux2 = tamaño_planetas[j];
-            tamaño_planetas[j] = tamaño_planetas[j + 1];
-            tamaño_planetas[j + 1] = aux2;
-
-            aux3 = distancia_tierra_planetas[j];
-            distancia_tierra_planetas[j] = distancia_tierra_planetas[j + 1];
-            distancia_tierra_planetas[j + 1] = aux3;
-        }
-    }
-}
-for (let i = 0; i < nombre_planetas.length; i++) {
-    document.write(nombre_planetas[i], '', '-', '');
-}
-document.write('Menor a mayor ');
-
-for (i = 1; i < nombre_planetas.length; i++) {
-    for (j = 0; j < (nombre_planetas.length - i); j++) {
-        if (distancia_sol_planetas[j] > distancia_sol_planetas[j + 1]) {
-            aux = distancia_sol_planetas[j];
-            distancia_sol_planetas[j] = distancia_sol_planetas[j + 1];
-            distancia_sol_planetas[j + 1] = aux;
-
-            aux1 = nombre_planetas[j];
-            nombre_planetas[j] = nombre_planetas[j + 1];
-            nombre_planetas[j + 1] = aux1;
-
-            aux2 = tamaño_planetas[j];
-            tamaño_planetas[j] = tamaño_planetas[j + 1];
-            tamaño_planetas[j + 1] = aux2;
-
-            aux3 = distancia_tierra_planetas[j];
-            distancia_tierra_planetas[j] = distancia_tierra_planetas[j + 1];
-            distancia_tierra_planetas[j + 1] = aux3;
-        }
-    }
-}
-for (let i = 0; i < nombre_planetas.length; i++) {
-    document.write(nombre_planetas[i], '', '-', '');
-}
-
-for (let i = 0; i < nombre_planetas.length; i++) {
-    bandera = distancia_tierra_planetas[i].includes('33');
-    if (bandera === true) {
-        document.write(nombre_planetas[i], '', 'tiene el número 33', '');
-    }
-}
-
-for (let i = 0; i < nombre_planetas.length; i++) {
-    if (distancia_tierra_planetas[i] === "pluton") {
-        document.write(nombre_planetas[i], '', 'es el planeta seleccionado', '');
-    }
-}
-*/
--- main2.js
+++ main2.js
@@ -1,124 +1,237 @@
-/*var btn_ver = document.getElementById("btn_ver").addEventListener("click", traerdatos);
-
+//var btn_ver = document.getElementById("btn_ver").addEventListener("click", verDatos);
 var btn_menor = document.getElementById("btn_menor").addEventListener("click", datos_menor);
-
 var btn_mayor = document.getElementById("btn_mayor").addEventListener("click", datos_mayor);
-
 var btn_33 = document.getElementById("btn_33").addEventListener("click", datos_33);
-
 var btn_nombre = document.getElementById("btn_nombre").addEventListener("click", buscar_nombre);
-
 var btn_orden_nombre = document.getElementById("btn_orden_nombre").addEventListener("click", datos_nombre);
+var planetas = []
 
-
-var nombre_planetas = new Array("Mercurio", "Venus", "Tierra", "Marte", "Jupiter", "Saturno", "Urando", 
-                                "Neptuno, Pluton","Haumea", "Makemake", "Eris", "Ceres");
-var distanciasSol=[57909175,108208930,149597870,227936640,778412010,1426725400,2870972200,
-                    4498252900,5906400000,52360000000,6850240000000,52360000000000,371446000000];
-var distanciaTierra = ["91,690,000","42,000,000","510,000,000","140,000,000","591,000,000",
-                        "1,200,000,000","2,543,164,000","4,500,000,000","6,000,000,000","5,108,320,000,000",
-                        "5,610,000,000","14,316,000,000","46,000,000,000"];
-var superficie = ["75,000,000","460,000,000","510,000,000","140,000,000","64,000,000,000","43,800,000,000",
-                    "8,130,000,000","7,700,000,000","17,000,000","1,952,160","1,450,000","2,372,000","2,850,000"]
-
-var masa = ["3,302×1023","4,8690×1024","5,9742×1024","6,4191×1023","1,8987×1027","5,6851×1026","8,6849×1025",
-"1,0244×1026","1,25×1022","4,2x1021","4x1021","3,75x1022","9,43×1020"]
-
-*/
-const planetas = []
-const planeta = {
-    nombre : "tierra",
-    tamano : 300000,
-    distanciaSol : 10000000,
-    distanciasTierra : 0
-}
 const planeta1 = {
-    nombre : "marte",
-    tamano : 100000,
-    distanciaSol : 3232000,
-    distanciasTierra : 1212121000
+    nombre: "mercurio",
+    imagen: "https://upload.wikimedia.org/wikipedia/commons/thumb/3/3f/Mercury_Globe-MESSENGER_mosaic_centered_at_0degN-0degE.jpg/105px-Mercury_Globe-MESSENGER_mosaic_centered_at_0degN-0degE.jpg",
+    distanciaSol: 57909175,
+    distanciaTierra: "91690000",
+    superficie: "75000000",
+    masa: "3,302×1023"
 }
-planetas.push(planeta)
+
+const planeta2 = {
+    nombre: "venus",
+    imagen: "https://upload.wikimedia.org/wikipedia/commons/thumb/e/e5/Venus-real_color.jpg/105px-Venus-real_color.jpg",
+    distanciaSol: 108208930,
+    distanciaTierra: "42000000",
+    superficie: "460000000",
+    masa: "4,8690×1024"
+}
+const planeta3 = {
+    nombre: "elbicho",
+    imagen: "https://yt3.ggpht.com/ytc/AAUvwngft30EJd_sVg4UzHUM6zHL8kT0uUVdPL1rZz1m=s900-c-k-c0x00ffffff-no-rj",
+    distanciaSol: 3000000000,
+    distanciaTierra: "31000000",
+    superficie: "187",
+    masa: "84"
+}
+const planeta4 = {
+    nombre: "tierra",
+    imagen: "https://upload.wikimedia.org/wikipedia/commons/thumb/1/1f/As08-16-2593.jpg/105px-As08-16-2593.jpg",
+    distanciaSol: 149597870,
+    distanciaTierra: "0",
+    superficie: "510000000",
+    masa: "5,9742×1024"
+}
+const planeta5 = {
+    nombre: "marte",
+    imagen: "https://upload.wikimedia.org/wikipedia/commons/thumb/0/02/OSIRIS_Mars_true_color.jpg/105px-OSIRIS_Mars_true_color.jpg",
+    distanciaSol: 227936640,
+    distanciaTierra: "69000000",
+    superficie: "140,000,000",
+    masa: "6,4191×1023"
+}
+const planeta6 = {
+    nombre: "jupiter",
+    imagen: "https://upload.wikimedia.org/wikipedia/commons/thumb/5/5a/Jupiter_by_Cassini-Huygens.jpg/953px-Jupiter_by_Cassini-Huygens.jpg",
+    distanciaSol: 778412010,
+    distanciaTierra: "591000000",
+    superficie: "64,000,000,000",
+    masa: "1,8987×1027"
+}
+const planeta7 = {
+    nombre: "saturno",
+    imagen: "https://upload.wikimedia.org/wikipedia/commons/thumb/c/c7/Saturn_during_Equinox.jpg/150px-Saturn_during_Equinox.jpg",
+    distanciaSol: 1426725400,
+    distanciaTierra: "1200000000",
+    superficie: "43,800,000,000",
+    masa: "5,6851×1026"
+}
+const planeta8 = {
+    nombre: "urano",
+    imagen: "https://upload.wikimedia.org/wikipedia/commons/thumb/b/bb/Uranus.jpg/105px-Uranus.jpg",
+    distanciaSol: 2870972200,
+    distanciaTierra: "2543164000",
+    superficie: "8,130,000,000",
+    masa: "8,6849×1025"
+}
+const planeta9 = {
+    nombre: "neptuno",
+    imagen: "https://upload.wikimedia.org/wikipedia/commons/thumb/6/63/Neptune_-_Voyager_2_%2829347980845%29_flatten_crop.jpg/105px-Neptune_-_Voyager_2_%2829347980845%29_flatten_crop.jpg",
+    distanciaSol: 4498252900,
+    distanciaTierra: "4500000000",
+    superficie: "7,700,000,000",
+    masa: "1,0244×1026"
+}
+const planeta10 = {
+    nombre: "pluton",
+    imagen: "https://upload.wikimedia.org/wikipedia/commons/thumb/e/ef/Pluto_in_True_Color_-_High-Res.jpg/250px-Pluto_in_True_Color_-_High-Res.jpg",
+    distanciaSol: 5906400000,
+    distanciaTierra: "6000000000",
+    superficie: "17,000,000",
+    masa: "1,25×1022"
+}
+const planeta11 = {
+    nombre: "haumea",
+    imagen: "https://upload.wikimedia.org/wikipedia/commons/thumb/9/95/Haumea%2C_Celestia.jpg/280px-Haumea%2C_Celestia.jpg",
+    distanciaSol: 52360000000,
+    distanciaTierra: "5108320000000",
+    superficie: "1,952,160",
+    masa: "4,2x1021"
+}
+const planeta12 = {
+    nombre: "makemake",
+    imagen: "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6c/Makemake_moon_Hubble_image_with_legend_%28cropped%29.jpg/280px-Makemake_moon_Hubble_image_with_legend_%28cropped%29.jpg",
+    distanciaSol: 6850240000000,
+    distanciaTierra: "5610000000",
+    superficie: "1,450,000",
+    masa: "4x1021"
+}
+const planeta13 = {
+    nombre: "eris",
+    imagen: "https://upload.wikimedia.org/wikipedia/commons/thumb/5/5b/Eris_and_dysnomia2.jpg/280px-Eris_and_dysnomia2.jpg",
+    distanciaSol: 52360000000000,
+    distanciaTierra: "14316000000",
+    superficie: "2,372,000",
+    masa: "3,75x1022"
+}
+const planeta14 = {
+    nombre: "ceres",
+    imagen: "https://upload.wikimedia.org/wikipedia/commons/thumb/5/58/Ceres_-_RC3_-_Haulani_Crater_%2822381131691%29.jpg/245px-Ceres_-_RC3_-_Haulani_Crater_%2822381131691%29.jpg",
+    distanciaSol: 371446000000,
+    distanciaTierra: "46000000000",
+    superficie: "2,850,000",
+    masa: "9,43×1020"
+}
 planetas.push(planeta1)
-console.log(planetas)
+planetas.push(planeta2)
+planetas.push(planeta3)
+planetas.push(planeta4)
+planetas.push(planeta5)
+planetas.push(planeta6)
+planetas.push(planeta7)
+planetas.push(planeta8)
+planetas.push(planeta9)
+planetas.push(planeta10)
+planetas.push(planeta11)
+planetas.push(planeta12)
+planetas.push(planeta13)
+planetas.push(planeta14)
+//console.log(planetas)
 
-let resultado_find1 = planetas.find(({nombre, distanciaSol})=> nombre.toLowerCase() == "marte")
-console.log(resultado_find1)
-
-/*
-//While
-let i=0;
-while(i<=2){
-    console.log(nombre_planetas[i])
-    i+=1;
+verDatos(planetas);
+function verDatos(d) {
+    let res = document.getElementById("res");
+    res.innerHTML = '';
+    for (let item of d) {
+        res.innerHTML += `
+        <tr>
+        <td>${item.nombre}</td>
+        <td><img src=${item.imagen}></td>
+        <td>${item.distanciaSol}</td>
+        <td>${item.distanciaTierra}</td>
+        <td>${item.superficie}</td>
+        <td>${item.masa}</td>
+    </tr>
+    `
+    }
+}
+function datos_nombre() {
+    const ordenNombre = planetas;
+    ordenNombre.sort((a, b) => (a.nombre < b.nombre) ? -1 : 1);
+    verDatos(ordenNombre);
 }
 
-//For
-for(let j=0; j<=4; j++){
-    console.log(distancias[j])
+function datos_menor() {
+    const planetaMenor = planetas;
+    planetaMenor.sort((a, b) => a.distanciaSol - b.distanciaSol)
+    verDatos(planetaMenor);
 }
+function datos_mayor() {
+    const planetaMayor = planetas;
+    planetaMayor.sort((a, b) => a.distanciaSol - b.distanciaSol)
+    planetaMayor.reverse();
+    verDatos(planetaMayor);
+}
+
+function buscar_nombre() {
+    var txt_name = document.getElementById("txt_name").value;
+    txt_name = txt_name.toLocaleLowerCase();
+    var name = planetas.find(({ nombre }) => nombre.toLowerCase() == txt_name)
+    res.innerHTML = '';
+    res.innerHTML += `
+    <tr>
+        <td>${name.nombre}</td>
+        <td><img src=${name.imagen}></td>
+        <td>${name.distanciaSol}</td>
+        <td>${name.distanciaTierra}</td>
+        <td>${name.superficie}</td>
+        <td>${name.masa}</td>
+    </tr>
+    `
+}
+function datos_33() {
+    var distancia1 = planetas.filter(({ distanciaTierra }) => distanciaTierra.includes(91))
+    verDatos(distancia1)
+}
+
+
+
 
 //For in
-for (const key in nombre_planetas) {
-    console.log(nombre_planetas[key]);
+for (const key in planetas) {
+    console.log(planetas[key]);
 }
 
 //For of
-for (const planeta of nombre_planetas) {
+for (const planeta of planetas) {
     console.log(planeta);
-}
-*/
-
-//Funcion iteradora
-function iterador(planeta, index, arreglo){
-    console.log("El planeta es" + planeta, index, arreglo)
-
 }
 
 //Funcion anonima
-const it = (planeta) => console.log("El planeta es" + planeta + "xd")
-
+const it = (planeta) => console.log("El planeta es " + planeta.nombre)
 //For each
-nombre_planetas.forEach(it)
+planetas.forEach(it)
 
-nombre_planetas.forEach(function (planeta){
-    console.log("El planeta es" + planeta)
-
+planetas.forEach(function (planeta) {
+    console.log("La superficie es " + planeta.superficie)
 })
 
 //Funcion anonima dentro de un arrow dentro de un foreach
-nombre_planetas.forEach((planeta) => console.log("El planeta es" + planeta + "xd"))
+planetas.forEach((planeta) => console.log("La masa es" + planeta.masa))
 
-
-/*
-//Funcion arrow
-let resta = () =>{
-    console.log(9-3);
-}
-resta()
-
-let sum = () => 4+3
-console.log(sum())
-*/
 //.map
-let resultado = nombre_planetas.map((planeta, index) => "El tamaño del planeta" + planeta + " es " + tamano[index]
-)
+let resultado = planetas.map((planeta, index) => "La superficie del planeta " + planeta.nombre + " es " + planeta.superficie[index])
 console.log(resultado)
 
-//.filter para todos los valores //numero 33
-let tamano_filter = tamano.filter((t)=>{
-    return t % 2 == 0
-})
-console.log(tamano_filter)
+//.filter para todos los valores
+let distanciaSol_filter = planetas.filter((t) => t.distanciaSol % 2 == 0)
+console.log(distanciaSol_filter)
 
-//.find para valor especifico //Nombre planeta
-let resultado_find = tamano.find((valor)=> valor % 2 == 0)
-console.log(resultado_find)
+//.find para valor especifico
+let distanciaTierra_find = planetas.find((valor) => valor.distanciaTierra % 2 == 0)
+console.log(distanciaTierra_find)
 
 //find index - si no lo encuentra -1, si lo encuentra muestra el index
-let resultado_findindex = nombre_planetas.findIndex((nombre)=> nombre.toLocaleLowerCase() == "tierra")
-console.log(resultado_findindex)
+let nombre_findindex = planetas.findIndex((nombre) => nombre.nombre.toLocaleLowerCase() == "tierra")
+console.log(nombre_findindex)
 
 //includes
-let resultado_includes = nombre_planetas.includes("Tierra")
+let resultado_includes = planetas.includes("tierra")
 console.log(resultado_includes)
